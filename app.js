209f6d092612461b877791ee5ffcf535cc2b31e8
Anders Lundsgård <anders@utvecklare.eu>
2016-02-29 12:47:37 +0100

    Added first use of mongoose

--- app.js
+++ app.js
@@ -1,60 +1,31 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Mongo and Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('test db opened');
+});
+var transSchema = mongoose.Schema({
+  name: String,
+  date: Date
+});
+var Trans = mongoose.model('Translation', transSchema);
+var startup = new Trans({ name: 'Startup', date: Date.now() });
+console.log(startup.name);
+startup.save();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Sample call: http://localhost:3000/?lat=59.2&lon=17.4&date=2016-01-14
+app.get('*', function(req, res) {
+  var log = new Trans({ name: 'LogCall', date: Date.now() });
+  console.log(log.name);
+  log.save();
+  console.log('Save done');
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+var port = 3000;
+app.listen(port);
+console.log('Listen on port: ' + port + '...');
