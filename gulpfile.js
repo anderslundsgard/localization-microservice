ca2496fe77c4cd796ed4ce13d81942a459808fa0
Anders Lundsgård <anders@utvecklare.eu>
2016-03-07 07:32:30 +0100

    Injecting bower js and css with wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-gulp.task('style', function() {
+gulp.task('style', function () {
     return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
@@ -11,4 +11,17 @@ gulp.task('style', function() {
         }))
         .pipe(jscs())
         .pipe(jscs.reporter());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: 'public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 });
