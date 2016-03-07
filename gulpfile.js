7681df5ad446ff08d2a4fc55d676f9585ff725eb
Anders Lundsgård <anders@utvecklare.eu>
2016-03-07 05:48:27 +0100

    Running jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 gulp.task('style', function(){
-    gulp.src(jsFiles)
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
-        }));
+        }))
+        .pipe(jscs());
 });
