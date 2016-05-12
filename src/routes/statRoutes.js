8fb141af2a05340e94136c5774b2eafa533f8f5f
Anders Lundsgård <anders@utvecklare.eu>
2016-05-12 12:54:10 +0200

    Just played with :id in route

--- src/routes/statRoutes.js
+++ src/routes/statRoutes.js
@@ -7,9 +7,10 @@ statRouter.route('/')
         res.send('stat');
     });
 
-statRouter.route('/single')
+// e.g: http://localhost:4500/stat/21
+statRouter.route('/:id')
     .get(function(req, res) {
-        res.send('Yey, single stat page, with route from different page!');
+        res.send('Yey, single stat page, with route from different page! id:' + req.params.id);
     });
 
 module.exports = statRouter;
